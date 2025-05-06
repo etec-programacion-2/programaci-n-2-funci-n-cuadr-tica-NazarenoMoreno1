77f61159ff52026ff58e19f3de29d89b999c8452
EchavarriaLander <l.echavarria@alumno.um.edu.ar>
2025-05-06 08:25:27 -0300

    Estructura básica y definición de coeficientes

---
+++ app/src/main/kotlin/org/example/App.kt
@@ -0,0 +1,12 @@
+package org.example
+
+fun main() {
+    val a = 1
+    val b = 3
+    val c = 2
+    if (a == 0){
+        println("El coeficiente a tiene que ser necesariamente distinto a 0")
+        return
+    }
+    println("$a x² + $b x + $c")
+}
